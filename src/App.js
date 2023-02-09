96743632a86fad0450d8c0f9bba1783b672584b8
limesoda314 <amiya017@ucr.edu>
2023-02-08 16:30:34 -0800

    Minor change

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Alphanumerical recognizer
         </p>
+        
+
+        <p>Dataset</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.nist.gov/itl/products-and-services/emnist-dataset"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          EMNIST dataset
         </a>
       </header>
     </div>
