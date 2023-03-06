23776d92a206e93b3470993f4dd5a83c957bbe2b
limesoda314 <amiya017@ucr.edu>
2023-03-05 16:55:27 -0800

    Basic canvas drawing functionality using react-canvas-draw library. Can undo, clear, and "save" an image. Still need to figure out functionality for saving image

--- client/src/components/pages/Drawing.js
+++ client/src/components/pages/Drawing.js
@@ -6,28 +6,95 @@ import {TextBox, Title} from "../styles/TextStyles";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
+import { useRef } from 'react';
+import CanvasDraw from "react-canvas-draw";
+import { Typography } from "@mui/material";
+
 function Drawing() {
+  const [show, toggleShow] = React.useState(true); // used in ToggleButton 
+  const canvasRef = useRef(null);
+  const [drawing, setDrawing] = React.useState();
+
+  const ExportDrawing = () => {
+    const base64 = canvasRef.current.canvasContainer.childNodes[1].toDataURL();
+    setDrawing(base64);
+  };
+
+  const ToggleButton = () => {
+    return (
+        <>
+          <Grid item xs={16}>
+            {show ?
+            <Button variant="contained"
+              onClick={() => toggleShow(!show) }
+            >
+              Start
+            </Button>
+            : <Box></Box>
+            }
+          </Grid>
+          <Grid item xs={16}>
+          {!show ?
+              <CanvasDraw 
+              ref={ canvasRef }
+              />
+          : <Box></Box>}
+          </Grid>
+          <Grid item xs={16}>
+          {!show ?
+            <Box>
+              <Button variant="contained" 
+                onClick={() => {
+                  canvasRef.current.undo();
+                }}>Undo
+              </Button>
+              <Button variant="contained"
+                onClick={() => {
+                  canvasRef.current.clear();
+              }}>Clear</Button>
+              <Button variant="contained"
+              onClick={ExportDrawing}
+              >Next</Button> 
+              <img src={drawing} alt="exported drawing" />
+            </Box>
+            : <Box></Box>}
+          </Grid>
+        </>
+    )
+  };
+
   return (
-      <Grid container spacing={2} columns={3}>
-      <Grid item xs={3}>
-        <Title component="h1">
-          Drawing
-        </Title>
-      </Grid>
-      <Grid item xs={3}>
-        <TextBox>
-          This is where the explanation would be. We could
-          also include an image, similar to the "HERO" image
-          that we include in the Home page.
-        </TextBox>
-      </Grid>
-      <Grid item xs={3}>
-        <Box textAlign="center">
-          <Button variant="contained">
-            Start
-          </Button>
-        </Box>
-      </Grid>
+      <Grid
+        container
+        alignItems="center"
+        textAlign="center"
+        justifyContent="center"
+        spacing={4}
+        columns={16}
+      >
+        <Grid item xs={16}>
+          <Title component="h1">
+            Drawing
+          </Title>
+        </Grid>
+        <Grid item xs={16}>
+          <TextBox>
+            This is where the explanation would be. We could
+            also include an image, similar to the "HERO" image
+            that we include in the Home page.
+          </TextBox>
+        </Grid>
+        {/* <Grid item xs={16}>
+          <Box textAlign="center">
+            <ToggleButton/> 
+          </Box>
+        </Grid> */}
+        {/* <Grid item xs={16}> */}
+        <ToggleButton />
+        {/* </Grid> */}
+        <Grid item alignItems="center" xs={16}>
+          <Typography>Hello world</Typography>
+        </Grid>
     </Grid>
   );
 }
